3e25dee2d26cae16cbe623008f1a2492be25ad03
Prathamesh Adhav <155577783+prathamesh3455@users.noreply.github.com>
2025-05-20 20:53:50 +0530

    Add files via upload

---
+++ #Check if a Number is Odd or Even.py
@@ -0,0 +1,16 @@
+#Check if a Number is Odd or Even
+x = 24 
+# Check the remainder dividing x by 2 is 0
+if x % 2 == 0:
+    print("Even")
+else:
+    print("Odd")
+
+# Checking another number    
+x = 7
+
+if x % 2 == 0:
+
+  print("Even")
+else:
+    print("Odd")
